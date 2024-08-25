fc5bf03fddcdd6415a18198b797a51bce2732fe3
ShivamDrh <shivamdrh15102003@gmail.com>
2024-08-25 12:13:31 +0530

    fabonise series

---
+++ fabo.java
@@ -0,0 +1,19 @@
+public class fabo {
+
+
+    public static void main(String[] args){
+       int first=0;
+        int second=1;
+        int third;
+        System.out.println("febonise series");
+        System.out.print(first);
+        System.out.print(","+second);
+        for(int i=0; i<12;i++){
+            third=first+second;
+            System.out.print(","+third);
+            first=second;
+            second=third;
+        }
+        }
+    }
+    
